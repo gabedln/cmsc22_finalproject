35150bc85bec0441ac916052d43f916dbb3f3f82
gabedln <mddeleon7@up.edu.ph>
2025-11-13 00:07:17 +0800

    Fixed bugs and background image not showing.

--- cmsc22_finalproj/src/application/Main.java
+++ cmsc22_finalproj/src/application/Main.java
@@ -10,10 +10,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			root.getStyleClass().add("root-border-pane");
+			BorderPane root = new BorderPane(); // initial welcome screen, prompts user to login
+			root.getStyleClass().add("border-pane");
 			Scene scene = new Scene(root,1024,576);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setTitle("Girl, Boy, Bakla, Tomboy Store");
 			stage.setScene(scene);
 			stage.show();
 		} catch(Exception e) {
